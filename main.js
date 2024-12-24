b0596ceae35cafc323243fe4de1a01ee71dfca2c
Hassan Khan <hassankhanwaseem15@gmail.com>
2024-12-24 16:15:47 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+alert("Hi, Welcome to the result calculator webpage.")
+
+var num1 = +prompt("waht are your marks in Maths out of 100")
+var num2 = +prompt("waht are your marks in English out of 100")
+var num3 = +prompt("waht are your marks in Urdu out of 100")
+var num4 = +prompt("waht are your marks in Biology out of 100")
+var num5 = +prompt("waht are your marks in Physics out of 100")
+// var totalmarks = prompt("waht are your Total marks")
+
+var total = num1 + num2 + num3 + num4 + num5; 
+alert(total)
